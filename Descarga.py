79875ceea95447144cf845ebe0fef4c605aba422
Mike <75444827+Mike538@users.noreply.github.com>
2022-04-26 08:19:11 -0500

    Create Descarga.py

---
+++ Descarga.py
@@ -0,0 +1,16 @@
+#operaciones básicas de dos numeros
+
+N1 = float(input"Ingrese el primer numero")
+print()
+N2 = float(input"Ingrese el segundo numero")
+print()
+suma = N1+N2
+Resta = N1-N2
+Multiplicación = N1*N2
+División = N1/N2
+print()
+print("El resultado de los dos numeros es el siguiente:" "\nsuma, suma, \Resta, Resta, \Multiplicación, Multiplicación,  \División, Division")
+print()
+print("Gracias por utilizar el programa")
+print()
+
